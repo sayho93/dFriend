c1cd31b2d4acbfd42c33cc8cc6129e2ffad8ae00
sayhoChun <fishcreek@naver.com>
2021-05-15 01:10:19 +0900

    match request, match status change, my match list added to WebRoute

--- shared/public/classes/WebRoute.php
+++ shared/public/classes/WebRoute.php
@@ -82,4 +82,59 @@ class WebRoute extends Routable {
         return $this->sendPush($title, $message, "", $token);
     }
 
+    function requestMatch(){
+        $myId = $_REQUEST["myId"];
+        $opponentId = $_REQUEST["opponentId"];
+
+        $chk = $this->getValue(
+            "SELECT COUNT(*) AS cnt 
+                FROM tblMatch 
+                WHERE (requestUserId = '{$myId}' AND receiverUserId = '{$opponentId}') OR (requestUserId = '{$opponentId}' AND receiverUserId = '{$myId}')",
+            "cnt"
+        );
+        if($chk != 0) return Routable::response(-1, "이미 신청된 매칭이 있습니다.");
+
+        $ins = "INSERT INTO tblMatch(`requestUserId`, `receiverUserId`) VALUES('{$myId}', '{$opponentId}')";
+        $this->update($ins);
+        $token = $this->getValue("SELECT pushToken FROM tblUser WHERE id = '{$opponentId}' LIMIT 1", "pushToken");
+        $nick = $this->getValue("SELECT nickname FROM tblUser WHERE id = '{$myId}' LIMIT 1", "nickname");
+        return $this->sendPush("매칭신청", "{$nick} 님으로부터 대화신청이 도착했습니다.", "", $token);
+    }
+
+    function updateMatchStat(){
+        $myId = $_REQUEST["myId"];
+        $opponentId = $_REQUEST["opponentId"];
+        $flag = $_REQUEST["flag"];
+        if($flag == "3"){
+            $tmp = $myId;
+            $myId = $opponentId;
+            $opponentId = $tmp;
+        }
+        $this->update(
+            "UPDATE tblMatch SET status = '{$flag}' WHERE requestUserId = '{$opponentId}' AND receiverUserId = '{$myId}'"
+        );
+        return Routable::response(1, "succ");
+    }
+
+    function myMatchStat(){
+        $myId = $_REQUEST['myId'];
+        $flag = $_REQUEST['flag'];
+        $columns = array("M.requestUserId", "M.receiverUserId");
+        if($flag == "req") $columns = array_reverse($columns);
+        $ins = "
+            SELECT
+                *,
+                U.id as id,
+                (SELECT `shortPath` FROM tblFile F WHERE F.`id`=U.profileId) AS profilePath,
+                (SELECT COUNT(*) FROM tblFollow WHERE followedId=U.`id`) AS followers,
+                (SELECT COUNT(*) FROM tblLike L WHERE L.boardId IN (SELECT `id` FROM tblBoard WHERE userKey=U.`id`)) AS likes,
+                (SELECT GROUP_CONCAT(characterId) FROM tblCharMap WHERE userId = U.id) AS characteristics,
+                (SELECT GROUP_CONCAT(description) FROM tblCharacter WHERE id IN (SELECT characterId FROM tblCharMap WHERE userId = U.id)) AS characteristicStr
+            FROM tblUser U JOIN tblMatch M ON U.id = {$columns[0]} 
+            WHERE {$columns[1]} = '{$myId}' AND status != 1;
+        ";
+        return Routable::response(1, "succ", $this->getArray($ins));
+    }
+
+
 }
